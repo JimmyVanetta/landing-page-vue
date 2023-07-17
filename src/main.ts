e5cfe8f2cab038be686ddad45d60a32058b85f57
James Vanetta <jvanetta@attorneylive.com>
2023-07-16 20:06:47 -0400

    add store

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import VueCompositionAPI from '@vue/composition-api'
+import store from './store'
 
 Vue.use(VueCompositionAPI)
 
@@ -10,5 +11,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
